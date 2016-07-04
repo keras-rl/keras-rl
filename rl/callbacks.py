2ab164aacb7543f5ed51db54fd0bbe515d4d0074
Matthias Plappert <matthiasplappert@me.com>
2016-07-04 11:32:20 +0200

    Start work on code clean-up

--- rl/callbacks.py
+++ rl/callbacks.py
@@ -1,26 +1,78 @@
 import timeit
 import numpy as np
 
-from keras.callbacks import Callback
+from keras.callbacks import Callback as KerasCallback, CallbackList as KerasCallbackList
+
+
+class Callback(KerasCallback):
+	def on_episode_begin(self, episode, logs={}):
+		pass
+
+	def on_episode_end(self, episode, logs={}):
+		pass
+
+	def on_step_begin(self, step, logs={}):
+		pass
+
+	def on_step_end(self, step, logs={}):
+		pass
+
+
+class CallbackList(KerasCallbackList):
+	def on_episode_begin(self, episode, logs={}):
+		for callback in self.callbacks:
+			# Check if callback supports the more appropriate `on_episode_begin` callback.
+			# If not, fall back to `on_epoch_begin` to be compatible with built-in Keras callbacks.
+			if callable(getattr(callback, 'on_episode_begin', None)):
+				callback.on_episode_begin(episode, logs=logs)
+			else:
+				callback.on_epoch_begin(episode, logs=logs)
+
+	def on_episode_end(self, episode, logs={}):
+		for callback in self.callbacks:
+			# Check if callback supports the more appropriate `on_episode_end` callback.
+			# If not, fall back to `on_epoch_end` to be compatible with built-in Keras callbacks.
+			if callable(getattr(callback, 'on_episode_end', None)):
+				callback.on_episode_end(episode, logs=logs)
+			else:
+				callback.on_epoch_end(episode, logs=logs)
+
+	def on_step_begin(self, step, logs={}):
+		for callback in self.callbacks:
+			# Check if callback supports the more appropriate `on_step_begin` callback.
+			# If not, fall back to `on_batch_begin` to be compatible with built-in Keras callbacks.
+			if callable(getattr(callback, 'on_step_begin', None)):
+				callback.on_step_begin(episode, logs=logs)
+			else:
+				callback.on_batch_begin(episode, logs=logs)
+
+	def on_step_end(self, step, logs={}):
+		for callback in self.callbacks:
+			# Check if callback supports the more appropriate `on_step_end` callback.
+			# If not, fall back to `on_batch_end` to be compatible with built-in Keras callbacks.
+			if callable(getattr(callback, 'on_step_end', None)):
+				callback.on_step_end(step, logs=logs)
+			else:
+				callback.on_batch_end(step, logs=logs)
 
 
 class TestLogger(Callback):
-	def on_epoch_end(self, epoch, logs):
-		template = 'Epoch {0}: reward: {1:.3f}'
+	def on_episode_end(self, episode, logs):
+		template = 'Episode {0}: reward: {1:.3f}'
 		variables = [
-			epoch,
+			episode,
 			logs['total_reward'],
 		]
 		print(template.format(*variables))
 
 
-class TrainEpochLogger(Callback):
+class TrainEpisodeLogger(Callback):
 	def __init__(self):
-		# Some algorithms compute multiple epochs at once since they are multi-threaded.
-		# We therefore use a dictionary that is indexed by the epoch to separate epochs
+		# Some algorithms compute multiple episodes at once since they are multi-threaded.
+		# We therefore use a dictionary that is indexed by the episode to separate episodes
 		# from each other.
-		self.epoch_start = {}
-		self.inputs = {}
+		self.episode_start = {}
+		self.observations = {}
 		self.rewards = {}
 		self.actions = {}
 		self.losses = {}
@@ -28,67 +80,67 @@ class TrainEpochLogger(Callback):
 
 	def on_train_begin(self, logs):
 		self.train_start = timeit.default_timer()
-		print('Training for {} epochs ...'.format(self.params['nb_epoch']))
+		print('Training for {} episodes ...'.format(self.params['nb_episodes']))
 
 	def on_train_end(self, logs):
 		duration = timeit.default_timer() - self.train_start
 		print('done, took {0:.3f} seconds'.format(duration))
 
-	def on_epoch_begin(self, epoch, logs):
-		self.epoch_start[epoch] = timeit.default_timer()
-		self.inputs[epoch] = []
-		self.rewards[epoch] = []
-		self.actions[epoch] = []
-		self.losses[epoch] = []
-		self.average_qs[epoch] = []
+	def on_episode_begin(self, episode, logs):
+		self.episode_start[episode] = timeit.default_timer()
+		self.observations[episode] = []
+		self.rewards[episode] = []
+		self.actions[episode] = []
+		self.losses[episode] = []
+		self.average_qs[episode] = []
 
-	def on_epoch_end(self, epoch, logs):
-		if len(self.average_qs[epoch]) == 0:
-			self.average_qs[epoch].append(0.)
-		if len(self.losses[epoch]) == 0:
-			self.losses[epoch].append(0.)
-		duration = timeit.default_timer() - self.epoch_start[epoch]
-		steps = len(self.inputs[epoch])
-		template = 'Epoch {0}: {1:.3f}s, steps: {2}, steps per second: {3:.0f}, reward: {4:.3f} [{5:.3f}, {6:.3f}], loss: {7:.3f}, avg q: {14:.3f} action: {8:.3f} [{9:.3f}, {10:.3f}], inputs: {11:.3f} [{12:.3f}, {13:.3f}]'
+	def on_episode_end(self, episode, logs):
+		if len(self.average_qs[episode]) == 0:
+			self.average_qs[episode].append(0.)
+		if len(self.losses[episode]) == 0:
+			self.losses[episode].append(0.)
+		duration = timeit.default_timer() - self.episode_start[episode]
+		steps = len(self.observations[episode])
+		template = 'Episode {0}: {1:.3f}s, steps: {2}, steps per second: {3:.0f}, reward: {4:.3f} [{5:.3f}, {6:.3f}], loss: {7:.3f}, avg q: {14:.3f} action: {8:.3f} [{9:.3f}, {10:.3f}], observations: {11:.3f} [{12:.3f}, {13:.3f}]'
 		variables = [
-			epoch,
+			episode,
 			duration,
 			steps,
 			float(steps) / duration,
-			np.sum(self.rewards[epoch]),
-			np.min(self.rewards[epoch]),
-			np.max(self.rewards[epoch]),
-			np.sum(self.losses[epoch]),
-			np.mean(self.actions[epoch]),
-			np.min(self.actions[epoch]),
-			np.max(self.actions[epoch]),
-			np.mean(self.inputs[epoch]),
-			np.min(self.inputs[epoch]),
-			np.max(self.inputs[epoch]),
-			np.mean(self.average_qs[epoch]),
+			np.sum(self.rewards[episode]),
+			np.min(self.rewards[episode]),
+			np.max(self.rewards[episode]),
+			np.sum(self.losses[episode]),
+			np.mean(self.actions[episode]),
+			np.min(self.actions[episode]),
+			np.max(self.actions[episode]),
+			np.mean(self.observations[episode]),
+			np.min(self.observations[episode]),
+			np.max(self.observations[episode]),
+			np.mean(self.average_qs[episode]),
 		]
 		print(template.format(*variables))
 
 		# Free up resources.
-		del self.epoch_start[epoch]
-		del self.inputs[epoch]
-		del self.rewards[epoch]
-		del self.actions[epoch]
-		del self.losses[epoch]
-		del self.average_qs[epoch]
+		del self.episode_start[episode]
+		del self.observations[episode]
+		del self.rewards[episode]
+		del self.actions[episode]
+		del self.losses[episode]
+		del self.average_qs[episode]
 
-	def on_batch_end(self, batch, logs):
-		epoch = logs['epoch']
-		self.inputs[epoch].append(np.concatenate(logs['input']))
-		self.rewards[epoch].append(logs['reward'])
-		self.actions[epoch].append(logs['action'])
+	def on_step_end(self, step, logs):
+		episode = logs['episode']
+		self.observations[episode].append(np.concatenate(logs['observation']))
+		self.rewards[episode].append(logs['reward'])
+		self.actions[episode].append(logs['action'])
 		
 		stats = logs['stats']
 		if not isinstance(stats, (list, tuple)):
 			stats = [stats]
-		self.losses[epoch].append(stats[0])
+		self.losses[episode].append(stats[0])
 		if len(stats) >= 2:
-			self.average_qs[epoch].append(stats[1])
+			self.average_qs[episode].append(stats[1])
 
 class TrainIntervalLogger(Callback):
 	def __init__(self, interval=10000):
@@ -98,7 +150,7 @@ class TrainIntervalLogger(Callback):
 
 	def reset(self):
 		self.interval_start = timeit.default_timer()
-		self.inputs = []
+		self.observations = []
 		self.rewards = []
 		self.actions = []
 		self.losses = []
@@ -106,7 +158,7 @@ class TrainIntervalLogger(Callback):
 
 	def on_train_begin(self, logs):
 		self.train_start = timeit.default_timer()
-		print('Training for {} epochs ...'.format(self.params['nb_epoch']))
+		print('Training for {} episodes ...'.format(self.params['nb_episode']))
 
 	def on_train_end(self, logs):
 		duration = timeit.default_timer() - self.train_start
@@ -118,8 +170,8 @@ class TrainIntervalLogger(Callback):
 		if len(self.losses) == 0:
 			self.losses.append(0.)
 		duration = timeit.default_timer() - self.interval_start
-		interval_steps = len(self.inputs)
-		template = '{0}: {1:.3f}s, interval steps: {2}, steps per second: {3:.0f}, reward: {4:.3f} [{5:.3f}, {6:.3f}], loss: {7:.3f}, avg q: {14:.3f} action: {8:.3f} [{9:.3f}, {10:.3f}], inputs: {11:.3f} [{12:.3f}, {13:.3f}]'
+		interval_steps = len(self.observations)
+		template = '{0}: {1:.3f}s, interval steps: {2}, steps per second: {3:.0f}, reward: {4:.3f} [{5:.3f}, {6:.3f}], loss: {7:.3f}, avg q: {14:.3f} action: {8:.3f} [{9:.3f}, {10:.3f}], observations: {11:.3f} [{12:.3f}, {13:.3f}]'
 		variables = [
 			self.steps,
 			duration,
@@ -132,14 +184,14 @@ class TrainIntervalLogger(Callback):
 			np.mean(self.actions),
 			np.min(self.actions),
 			np.max(self.actions),
-			np.mean(self.inputs),
-			np.min(self.inputs),
-			np.max(self.inputs),
+			np.mean(self.observations),
+			np.min(self.observations),
+			np.max(self.observations),
 			np.mean(self.average_qs),
 		]
 		print(template.format(*variables))
 
-	def on_batch_end(self, batch, logs):
+	def on_step_end(self, step, logs):
 		if self.steps % self.interval == 0:
 			if self.steps > 0:
 				self.print_report()
@@ -147,7 +199,7 @@ class TrainIntervalLogger(Callback):
 
 		# Book-keeping.
 		self.steps += 1
-		self.inputs.append(np.concatenate(logs['input']))
+		self.observations.append(np.concatenate(logs['observation']))
 		self.rewards.append(logs['reward'])
 		self.actions.append(logs['action'])
 		stats = logs['stats']
@@ -156,3 +208,8 @@ class TrainIntervalLogger(Callback):
 		self.losses.append(stats[0])
 		if len(stats) >= 2:
 			self.average_qs.append(stats[1])
+
+
+class Visualizer(Callback):
+	def on_step_end(self, step, logs):
+		self.params['env'].render(mode='human')
---
+++ rl/core.py
@@ -0,0 +1,247 @@
+import numpy as np
+
+from rl.callbacks import TestLogger, TrainEpisodeLogger, Visualizer, CallbackList
+
+
+class Agent(object):
+    def fit(self, env, nb_episodes=100, action_repetition=1, callbacks=[], verbose=1, visualize=False):
+        if action_repetition < 1:
+            raise ValueError('action_repetition must be >= 1, is {}'.format(action_repetition))
+
+        self.training = True
+
+        if verbose > 0:
+            callbacks += [TrainEpisodeLogger()]
+        if visualize:
+            callbacks += [Visualizer()]
+        callbacks = CallbackList(callbacks)
+        callbacks._set_model(self)
+        callbacks._set_params({
+            'nb_episodes': nb_episodes,
+            'env': env,
+        })
+        callbacks.on_train_begin()
+
+        for episode_idx in xrange(nb_episodes):
+            callbacks.on_episode_begin(episode_idx)
+            step_idx = 0
+            total_reward = 0.
+
+            # Obtain the initial observation by resetting the environment.
+            self.reset()
+            observation = env.reset()
+            assert observation is not None
+
+            # Run the episode until we're done.
+            done = False
+            while not done:
+                callbacks.on_step_begin(step_idx)
+                
+                # This is were all of the work happens. We first perceive and compute the action
+                # (forward step) and then use the reward to improve (backward step).
+                action = self.forward(observation)
+                reward = 0.
+                for _ in xrange(action_repetition):
+                    observation, r, d, _ = env.step(action)
+                    reward += r
+                    if d:
+                        done = True
+                        break
+                stats = self.backward(reward, terminal=done)
+                total_reward += reward
+                
+                step_logs = {
+                    'action': action,
+                    'observation': observation,
+                    'reward': reward,
+                    'stats': stats,
+                    'episode': episode_idx,
+                }
+                callbacks.on_step_end(step_idx, step_logs)
+                step_idx += 1
+            episode_logs = {
+                'total_reward': total_reward,
+            }
+            callbacks.on_episode_end(episode_idx, episode_logs)
+        callbacks.on_train_end()
+
+    def test(self, env, nb_episodes=1, action_repetition=1, callbacks=[], visualize=True):
+        # TODO: implement callbacks. The Keras callbacks do not really fit, maybe we need to extend.
+        if action_repetition < 1:
+            raise ValueError('action_repetition must be >= 1, is {}'.format(action_repetition))
+
+        self.training = False
+
+        callbacks += [TestLogger()]
+        if visualize:
+            callbacks += [Visualizer()]
+        callbacks = CallbackList(callbacks)
+        callbacks._set_model(self)
+        callbacks._set_params({
+            'nb_episode': nb_episode,
+            'env': env,
+        })
+
+        for episode_idx in xrange(nb_episode):
+            callbacks.on_episode_begin(episode_idx)
+            total_reward = 0.
+            step_idx = 0
+
+            # Obtain the initial observation by resetting the environment.
+            self.reset()
+            observation = env.reset()
+            assert observation is not None
+
+            # Run the episode until we're done.
+            done = False
+            while not done:
+                callbacks.on_step_begin(step_idx)
+
+                action = self.forward(observation)
+                reward = 0.
+                for _ in xrange(action_repetition):
+                    observation, r, d, _ = env.step(action)
+                    reward += r
+                    if d:
+                        done = True
+                        break
+                self.backward(reward, terminal=done)
+                total_reward += reward
+                
+                callbacks.on_step_end(step_idx)
+                step_idx += 1
+            episode_logs = {
+                'total_reward': total_reward,
+            }
+            callbacks.on_episode_end(episode_idx, episode_logs)
+            game.reset()
+            self.reset_states()
+
+    def reset_states(self):
+        pass
+
+    def forward(self, current_input):
+        raise NotImplementedError()
+
+    def backward(self, reward, terminal=False):
+        raise NotImplementedError()
+
+    def load_weights(self, filepath):
+        raise NotImplementedError()
+
+    def save_weights(self, filepath, overwrite=False):
+        raise NotImplementedError()
+
+
+# Note: the API of the `Env` and `Space` classes are taken from the OpenAI Gym implementation.
+# https://github.com/openai/gym/blob/master/gym/core.py
+
+
+class Env(object):
+    """The abstract environment class that is used by all agents. This class has the exact
+    same API that OpenAI Gym uses so that integrating with it is trivial. In contrast to the
+    OpenAI Gym implementation, this class only defines the abstract methods without any actual
+    implementation.
+    """
+    reward_range = (-np.inf, np.inf)
+    action_space = None
+    observation_space = None
+
+    def step(self, action):
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+        Accepts an action and returns a tuple (observation, reward, done, info).
+        Args:
+            action (object): an action provided by the environment
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
+        raise NotImplementedError()
+
+    def reset(self):
+        """
+        Resets the state of the environment and returns an initial observation.
+        Returns:
+            observation (object): the initial observation of the space. (Initial reward is assumed to be 0.)
+        """
+        raise NotImplementedError()
+
+    def render(self, mode='human', close=False):
+        """Renders the environment.
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.) By convention,
+        if mode is:
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation. The text can include newlines
+          and ANSI escape sequences (e.g. for colors).
+        Note:
+            Make sure that your class's metadata 'render.modes' key includes
+              the list of supported modes. It's recommended to call super()
+              in implementations to use the functionality of this method.
+        Args:
+            mode (str): the mode to render with
+            close (bool): close all open renderings
+        """
+        raise NotImplementedError()
+
+    def close(self):
+        """Override in your subclass to perform any necessary cleanup.
+        Environments will automatically close() themselves when
+        garbage collected or when the program exits.
+        """
+        raise NotImplementedError()
+
+    def seed(self, seed=None):
+        """Sets the seed for this env's random number generator(s).
+        Note:
+            Some environments use multiple pseudorandom number generators.
+            We want to capture all such seeds used in order to ensure that
+            there aren't accidental correlations between multiple generators.
+        Returns:
+            list<bigint>: Returns the list of seeds used in this env's random
+              number generators. The first value in the list should be the
+              "main" seed, or the value which a reproducer should pass to
+              'seed'. Often, the main seed equals the provided 'seed', but
+              this won't be true if seed=None, for example.
+        """
+        raise NotImplementedError()
+
+    def configure(self, *args, **kwargs):
+        """Provides runtime configuration to the environment.
+        This configuration should consist of data that tells your
+        environment how to run (such as an address of a remote server,
+        or path to your ImageNet data). It should not affect the
+        semantics of the environment.
+        """
+        raise NotImplementedError()
+
+    def __del__(self):
+        self.close()
+
+    def __str__(self):
+        return '<{} instance>'.format(type(self).__name__)
+
+
+class Space(object):
+    """Abstract model for a space that is used for the state and action spaces. This class has the
+    exact same API that OpenAI Gym uses so that integrating with it is trivial.
+    """
+
+    def sample(self, seed=None):
+        """Uniformly randomly sample a random element of this space.
+        """
+        raise NotImplementedError()
+
+    def contains(self, x):
+        """Return boolean specifying if x is a valid member of this space
+        """
+        raise NotImplementedError()

421c5fb4bd96d4ec3aaead5bc175f61754aea8f3
Alan Nichol <amn41@cam.ac.uk>
2016-08-19 15:10:41 +0100

    still debugging a bit but working better

--- examples/cem_cartpole.py
+++ examples/cem_cartpole.py
@@ -20,20 +20,20 @@ nb_actions = env.action_space.n
 obs_dim = env.observation_space.shape[0]
 
 # Option 1 : Simple linear model
-#model = Sequential()
-#model.add(Dense(nb_actions,input_dim=obs_dim))
-#model.add(Activation('linear'))
+model = Sequential()
+model.add(Dense(nb_actions,input_dim=obs_dim))
+model.add(Activation('linear'))
 
 # Option 2: deep network
-model = Sequential()
-model.add(Dense(16,input_dim=obs_dim))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(nb_actions))
-model.add(Activation('linear'))
+# model = Sequential()
+# model.add(Dense(16,input_dim=obs_dim))
+# model.add(Activation('relu'))
+# model.add(Dense(16))
+# model.add(Activation('relu'))
+# model.add(Dense(16))
+# model.add(Activation('relu'))
+# model.add(Dense(nb_actions))
+# model.add(Activation('linear'))
 
 
 print(model.summary())
@@ -43,13 +43,14 @@ print(model.summary())
 # even the metrics!
 memory = EpisodeParameterMemory(limit=1000)
 
-cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10)
+cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory,
+          batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
 cem.compile()
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-cem.fit(env, nb_steps=50000, visualize=False, verbose=2)
+cem.fit(env, nb_steps=100000, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
--- rl/agents/cem.py
+++ rl/agents/cem.py
@@ -12,7 +12,7 @@ from rl.util import *
 
 class CEMAgent(Agent):
     def __init__(self, model, nb_actions, memory, window_length=1,
-                 gamma=.99, batch_size=6, nb_steps_warmup=0, train_interval=1, memory_interval=1 ):
+                 gamma=.99, batch_size=50, nb_steps_warmup=1000, train_interval=50, elite_frac=0.05,memory_interval=1 ):
 
 
         super(CEMAgent, self).__init__()
@@ -21,6 +21,8 @@ class CEMAgent(Agent):
         self.nb_actions = nb_actions
         self.gamma = gamma
         self.batch_size = batch_size
+        self.elite_frac = elite_frac
+        self.num_best = int(self.batch_size*self.elite_frac)
         self.nb_steps_warmup = nb_steps_warmup
         self.train_interval = train_interval
         self.memory_interval = memory_interval
@@ -38,10 +40,8 @@ class CEMAgent(Agent):
         self.sizes = [w.size for w in model.get_weights()]
         self.num_weights = sum(self.sizes)
 
-        print(self.sizes)
 
         self.update_theta(None)
-
 
         # State.
         self.compiled = False
@@ -59,6 +59,16 @@ class CEMAgent(Agent):
     def save_weights(self, filepath, overwrite=False):
         self.model.save_weights(filepath, overwrite=overwrite)
 
+    def get_flat_weights(self):
+        flat_weights = np.zeros(self.num_weights)
+        weights = self.model.get_weights()
+        pos=0
+        for i_layer, size in enumerate(self.sizes):
+            flat_weights[pos:pos+size] = weights[i_layer].flatten()
+            pos += size
+
+        return flat_weights
+
     def reset_states(self):
         self.recent_action = None
         self.recent_observations = deque(maxlen=self.window_length)
@@ -74,18 +84,21 @@ class CEMAgent(Agent):
         if (theta is not None):
             self.theta = theta
         else:
-            self.theta = np.random.randn(self.num_weights*2)
+            self.theta = np.hstack( (
+              np.random.randn(self.num_weights), # means
+              np.random.randn(self.num_weights)  # covariances
+            ))
 
     def choose_weights(self):
 
         mean = self.theta[:self.num_weights]
         cov = self.theta[self.num_weights:]
-        params = np.square(cov) * np.random.randn(self.num_weights) +  mean
+        weights_flat = np.square(cov) * np.random.randn(self.num_weights) +  mean
 
         sampled_weights = []
         pos=0
         for i_layer, size in enumerate(self.sizes):
-            arr = params[pos:pos+size].reshape(self.shapes[i_layer])
+            arr = weights_flat[pos:pos+size].reshape(self.shapes[i_layer])
             sampled_weights.append(arr)
             pos += size
 
@@ -93,6 +106,7 @@ class CEMAgent(Agent):
 
     def forward(self, observation):
         # Select an action.
+
         while len(self.recent_observations) < self.recent_observations.maxlen:
             # Not enough data, fill the recent_observations queue with copies of the current input.
             # This allows us to immediately perform a policy action instead of falling back to random
@@ -105,7 +119,7 @@ class CEMAgent(Agent):
         # Book-keeping.
         self.recent_observations.append(observation)
         self.recent_action = action
-        self.recent_params.append(self.theta)
+        self.recent_params.append(self.get_flat_weights())
 
         return action
 
@@ -125,11 +139,22 @@ class CEMAgent(Agent):
             self.memory.finalise_episode(self.recent_params[-1])
             self.episode += 1
 
-            if (self.episode % self.batch_size == 0):
+            if (self.step > self.nb_steps_warmup and self.episode % self.train_interval == 0):
                 params , reward_totals = self.memory.sample(self.batch_size)
-                best_idx = np.argsort(np.array(reward_totals))
-                new_params = np.mean(np.vstack(params[i] for i in best_idx),axis=0)
-                assert new_params.shape == params[0].shape
+                best_idx = np.argsort(np.array(reward_totals))[-self.num_best:]
+                best = np.vstack(params[i] for i in best_idx)
+
+                assert (not np.isnan(best).any())
+                mean = np.mean(best,axis=0)
+                print("new mean")
+                print(mean)
+                print("rewards")
+                print([reward_totals[i] for i in best_idx])
+                cov = np.std(best,axis=0)
+                print("new cov")
+                print(cov)
+                new_params = np.hstack((mean,cov))
+                assert new_params.shape == self.theta.shape
                 self.update_theta(new_params)
 
             self.choose_weights()

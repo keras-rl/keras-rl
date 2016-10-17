4d8eecd3ce0415182d2555cfed4a9d8dde95f04f
Matthias Plappert <matthiasplappert@me.com>
2016-10-17 17:48:54 +0200

    Clean up CEM and refactor memory code to be less redundant

--- examples/cem_cartpole.py
+++ examples/cem_cartpole.py
@@ -21,12 +21,14 @@ obs_dim = env.observation_space.shape[0]
 
 # Option 1 : Simple model
 model = Sequential()
-model.add(Dense(nb_actions,input_dim=obs_dim))
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(nb_actions))
 model.add(Activation('softmax'))
 
 # Option 2: deep network
 # model = Sequential()
-# model.add(Dense(16,input_dim=obs_dim))
+# model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+# model.add(Dense(16))
 # model.add(Activation('relu'))
 # model.add(Dense(16))
 # model.add(Activation('relu'))
@@ -41,7 +43,7 @@ print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = EpisodeParameterMemory(limit=1000,max_episode_steps=200)
+memory = EpisodeParameterMemory(limit=1000, window_length=1)
 
 cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory,
                batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
--- rl/memory.py
+++ rl/memory.py
@@ -55,11 +55,47 @@ class RingBuffer(object):
             raise RuntimeError()
         self.data[(self.start + self.length - 1) % self.maxlen] = v
 
-class SequentialMemory(object):
-    def __init__(self, limit, window_length, ignore_episode_boundaries=False):
-        self.limit = limit
+
+class Memory(object):
+    def __init__(self, window_length, ignore_episode_boundaries=False):
         self.window_length = window_length
         self.ignore_episode_boundaries = ignore_episode_boundaries
+
+        self.recent_observations = deque(maxlen=window_length)
+        self.recent_terminals = deque(maxlen=window_length)
+
+    def sample(self, batch_size, batch_idxs=None):
+        raise NotImplementedError()
+
+    def append(self, observation, action, reward, terminal, training=True):
+        self.recent_observations.append(np.array(observation))
+        self.recent_terminals.append(terminal)
+
+    def get_recent_state(self, current_observation):
+        # This code is slightly complicated by the fact that subsequent observations might be
+        # from different episodes. We ensure that an experience never spans multiple episodes.
+        # This is probably not that important in practice but it seems cleaner.
+        state = [np.array(current_observation)]
+        idx = len(self.recent_observations) - 1
+        for offset in range(0, self.window_length - 1):
+            current_idx = idx - offset
+            current_terminal = self.recent_terminals[current_idx - 1] if current_idx - 1 >= 0 else False
+            if current_idx < 0 or (not self.ignore_episode_boundaries and current_terminal):
+                # The previously handled observation was terminal, don't add the current one.
+                # Otherwise we would leak into a different episode.
+                break
+            state.insert(0, self.recent_observations[current_idx])
+        while len(state) < self.window_length:
+            state.insert(0, np.zeros(state[0].shape))
+        state = np.array(state)
+        assert state.shape[0] == self.window_length
+        return state
+
+class SequentialMemory(Memory):
+    def __init__(self, limit, **kwargs):
+        super(SequentialMemory, self).__init__(**kwargs)
+        
+        self.limit = limit
 
         # Do not use deque to implement the memory. This data structure may seem convenient but
         # it is way too slow on random access. Instead, we use our own ring buffer implementation.
@@ -67,8 +103,6 @@ class SequentialMemory(object):
         self.rewards = RingBuffer(limit)
         self.terminals = RingBuffer(limit)
         self.observations = RingBuffer(limit)
-        self.recent_observations = deque(maxlen=window_length)
-        self.recent_terminals = deque(maxlen=window_length)
 
     def sample(self, batch_size, batch_idxs=None):
         if batch_idxs is None:
@@ -123,27 +157,9 @@ class SequentialMemory(object):
         assert len(experiences) == batch_size
         return experiences
 
-    def get_recent_state(self, current_observation):
-        # This code is slightly complicated by the fact that subsequent observations might be
-        # from different episodes. We ensure that an experience never spans multiple episodes.
-        # This is probably not that important in practice but it seems cleaner.
-        state = [np.array(current_observation)]
-        idx = len(self.recent_observations) - 1
-        for offset in range(0, self.window_length - 1):
-            current_idx = idx - offset
-            current_terminal = self.recent_terminals[current_idx - 1] if current_idx - 1 >= 0 else False
-            if current_idx < 0 or (not self.ignore_episode_boundaries and current_terminal):
-                # The previously handled observation was terminal, don't add the current one.
-                # Otherwise we would leak into a different episode.
-                break
-            state.insert(0, self.recent_observations[current_idx])
-        while len(state) < self.window_length:
-            state.insert(0, np.zeros(state[0].shape))
-        state = np.array(state)
-        assert state.shape[0] == self.window_length
-        return state
-
     def append(self, observation, action, reward, terminal, training=True):
+        super(SequentialMemory, self).append(observation, action, reward, terminal, training=training)
+        
         # This needs to be understood as follows: in `observation`, take `action`, obtain `reward`
         # and weather the next state is `terminal` or not.
         if training:
@@ -151,59 +167,54 @@ class SequentialMemory(object):
             self.actions.append(action)
             self.rewards.append(reward)
             self.terminals.append(terminal)
-        self.recent_observations.append(np.array(observation))
-        self.recent_terminals.append(terminal)
 
     @property
     def nb_entries(self):
         return len(self.observations)
 
     def get_config(self):
-        config = {
-            'limit': self.limit,
-            'window_size': self.window_size,
-            'ignore_episode_boundaries': self.ignore_episode_boundaries,
-        }
+        config = super(SequentialMemory, self).get_config()
+        config['limit'] = self.limit
         return config
 
 
-class EpisodeParameterMemory(object):
-    def __init__(self, limit, max_episode_steps):
+class EpisodeParameterMemory(Memory):
+    def __init__(self, limit, **kwargs):
+        super(EpisodeParameterMemory, self).__init__(**kwargs)
         self.limit = limit
-        self.max_episode_steps = max_episode_steps
 
         self.params = RingBuffer(limit)
-        self.intermediate_rewards = RingBuffer(self.max_episode_steps)
-        self.reward_totals = RingBuffer(limit)
+        self.intermediate_rewards = []
+        self.total_rewards = RingBuffer(limit)
 
     def sample(self, batch_size, batch_idxs=None):
         if batch_idxs is None:
             batch_idxs = sample_batch_indexes(0, self.nb_entries, size=batch_size)
         assert len(batch_idxs) == batch_size
 
-        params = []
-        reward_totals = []
+        batch_params = []
+        batch_total_rewards = []
         for idx in batch_idxs:
-            params.append(self.params[idx])
-            reward_totals.append(self.reward_totals[idx])
-        return params, reward_totals
+            batch_params.append(self.params[idx])
+            batch_total_rewards.append(self.total_rewards[idx])
+        return batch_params, batch_total_rewards
 
-    def append(self,reward):
-        self.intermediate_rewards.append(reward)
+    def append(self, observation, action, reward, terminal, training=True):
+        super(EpisodeParameterMemory, self).append(observation, action, reward, terminal, training=training)
+        if training:
+            self.intermediate_rewards.append(reward)
 
-    def finalise_episode(self,params):
-        total_reward = sum([d for d in self.intermediate_rewards.data if d])
-        self.reward_totals.append(total_reward)
+    def finalize_episode(self, params):
+        total_reward = sum(self.intermediate_rewards)
+        self.total_rewards.append(total_reward)
         self.params.append(params)
-        self.intermediate_rewards = RingBuffer(self.max_episode_steps)
+        self.intermediate_rewards = []
 
     @property
     def nb_entries(self):
-        return len(self.reward_totals)
+        return len(self.total_rewards)
 
     def get_config(self):
-        config = {
-            'limit': self.limit,
-            'max_episode_steps': self.max_episode_steps,
-        }
+        config = super(SequentialMemory, self).get_config()
+        config['limit'] = self.limit
         return config

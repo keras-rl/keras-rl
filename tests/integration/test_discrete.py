c74455193c34e3e5abd72e59fdd6f8d68427264e
Matthias Plappert <matthiasplappert@me.com>
2016-12-08 17:06:08 +0100

    Add integration test for recurrent DQN

--- tests/integration/test_discrete.py
+++ tests/integration/test_discrete.py
@@ -5,12 +5,12 @@ from numpy.testing import assert_allclose
 from gym.envs.debugging.two_round_deterministic_reward import TwoRoundDeterministicRewardEnv
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, TimeDistributed, LSTM
 from keras.optimizers import Adam
 
 from rl.agents import DQNAgent, CEMAgent
 from rl.policy import EpsGreedyQPolicy
-from rl.memory import SequentialMemory, EpisodeParameterMemory
+from rl.memory import SequentialMemory, EpisodeParameterMemory, EpisodicMemory
 
 
 def test_dqn():
@@ -57,6 +57,38 @@ def test_double_dqn():
     policy = EpsGreedyQPolicy(eps=.1)
     dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=50,
                    target_model_update=1e-1, policy=policy, enable_double_dqn=True)
+    dqn.compile(Adam(lr=1e-3))
+
+    dqn.fit(env, nb_steps=2000, visualize=False, verbose=0)
+    policy.eps = 0.
+    h = dqn.test(env, nb_episodes=20, visualize=False)
+    assert_allclose(np.mean(h.history['episode_reward']), 3.)
+
+
+def test_recurrent_dqn():
+    env = TwoRoundDeterministicRewardEnv()
+    np.random.seed(123)
+    env.seed(123)
+    random.seed(123)
+    nb_actions = env.action_space.n
+    batch_size = np.random.random_integers(10, 20)
+
+    # Next, we build a very simple model.
+    policy_model = Sequential()
+    policy_model.add(LSTM(16, return_sequences=True, stateful=True, batch_input_shape=(1, None, 1,)))
+    policy_model.add(TimeDistributed(Dense(nb_actions)))
+    policy_model.add(Activation('linear'))
+
+    model = Sequential()
+    model.add(LSTM(16, return_sequences=True, stateful=True, batch_input_shape=(batch_size, None, 1,)))
+    model.add(TimeDistributed(Dense(nb_actions)))
+    model.add(Activation('linear'))
+
+    memory = EpisodicMemory(limit=1000, window_length=1)
+    policy = EpsGreedyQPolicy(eps=.1)
+    dqn = DQNAgent(model=model, policy_model=policy_model, nb_actions=nb_actions, memory=memory,
+                   nb_steps_warmup=50, target_model_update=1e-1, policy=policy, enable_double_dqn=False,
+                   batch_size=batch_size)
     dqn.compile(Adam(lr=1e-3))
 
     dqn.fit(env, nb_steps=2000, visualize=False, verbose=0)

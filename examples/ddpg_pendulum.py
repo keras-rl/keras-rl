cbd7ad8f927a57cd601c9ad11e2b37a71490c878
Benjamin Ellenberger <benelot@users.noreply.github.com>
2017-01-28 22:48:50 +0100

    Add correct dimensionality for the OUProcess.
    This does not fix this very example but solves a problem as soon as somebody builds an example based on this which has more than one dimension.

--- examples/ddpg_pendulum.py
+++ examples/ddpg_pendulum.py
@@ -52,7 +52,7 @@ print(critic.summary())
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3)
+random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
 agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
                   memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
                   random_process=random_process, gamma=.99, target_model_update=1e-3)

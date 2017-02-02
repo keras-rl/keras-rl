93c90b154f8d84a1783733acd44fdb1ce38d975b
Matthias Plappert <matthiasplappert@me.com>
2017-02-02 09:40:43 +0100

    Prepare release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl',
-      version='0.2.0rc1',
+      version='0.2.0',
       description='Deep Reinforcement Learning for Keras',
       author='Matthias Plappert',
       author_email='matthiasplappert@me.com',
       url='https://github.com/matthiasplappert/keras-rl',
-      download_url='https://github.com/matthiasplappert/keras-rl/tarball/0.1',
+      download_url='https://github.com/matthiasplappert/keras-rl/archive/v0.2.0.tar.gz',
       license='MIT',
       install_requires=['keras>=1.0.7'],
       extras_require={

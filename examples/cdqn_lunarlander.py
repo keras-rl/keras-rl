9598e07d648d2ecd7f975b4129b7182ea307bf57
Ryan Hope <rmh3093@gmail.com>
2017-03-27 12:03:48 -0400

    add a lunar lander example since it supports multiple continuous actions

---
+++ examples/cdqn_lunarlander.py
@@ -0,0 +1,86 @@
+import numpy as np
+import gym
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Input
+from keras.optimizers import Adam
+
+from rl.agents import ContinuousDQNAgent
+from rl.memory import SequentialMemory
+from rl.random import OrnsteinUhlenbeckProcess
+from rl.core import Processor
+from rl.keras_future import concatenate, Model
+
+class LunarLanderContinuousProcessor(Processor):
+    def process_action(self, action):
+        return np.array(map(lambda x: min(max(x,-1),1), action))
+
+ENV_NAME = 'LunarLanderContinuous-v2'
+gym.undo_logger_setup()
+
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.shape[0]
+
+# Build all necessary models: V, mu, and L networks.
+V_model = Sequential()
+V_model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(1))
+V_model.add(Activation('linear'))
+print(V_model.summary())
+
+mu_model = Sequential()
+mu_model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(nb_actions))
+mu_model.add(Activation('linear'))
+print(mu_model.summary())
+
+action_input = Input(shape=(nb_actions,), name='action_input')
+observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
+x = concatenate([action_input, Flatten()(observation_input)])
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(((nb_actions * nb_actions + nb_actions) / 2))(x)
+x = Activation('linear')(x)
+L_model = Model(input=[action_input, observation_input], output=x)
+print(L_model.summary())
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+processor = LunarLanderContinuousProcessor()
+memory = SequentialMemory(limit=100000, window_length=1)
+random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
+agent = ContinuousDQNAgent(nb_actions=nb_actions, V_model=V_model, L_model=L_model, mu_model=mu_model,
+                           memory=memory, nb_steps_warmup=100, random_process=random_process,
+                           gamma=.99, target_model_update=1e-3, processor=processor)
+agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+agent.fit(env, nb_steps=50000, visualize=True, verbose=2, nb_max_episode_steps=200)
+
+# After training is done, we save the final weights.
+agent.save_weights('cdqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+agent.test(env, nb_episodes=10, visualize=True, nb_max_episode_steps=200)

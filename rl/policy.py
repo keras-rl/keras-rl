af9d33284246a18d0f1d08f8246c6ff3de389ce2
Ryan Hope <rmh3093@gmail.com>
2017-07-17 11:39:19 -0400

    Add Max-Boltzman policy, combination of eps-greedy and boltzman
    Wiering, M.: Explorations in Efficient Reinforcement Learning. PhD
    thesis, University of Amserdam, Amsterdam (1999)

--- rl/policy.py
+++ rl/policy.py
@@ -119,3 +119,30 @@ class BoltzmannQPolicy(Policy):
         config['tau'] = self.tau
         config['clip'] = self.clip
         return config
+
+class MaxBoltzmannQPolicy(Policy):
+    def __init__(self, eps=.1, tau=1., clip=(-500., 500.)):
+        super(MaxBoltzmannQPolicy, self).__init__()
+        self.eps = eps
+        self.tau = tau
+        self.clip = clip
+
+    def select_action(self, q_values):
+        assert q_values.ndim == 1
+        q_values = q_values.astype('float64')
+        nb_actions = q_values.shape[0]
+
+        if np.random.uniform() < self.eps:
+            exp_values = np.exp(np.clip(q_values / self.tau, self.clip[0], self.clip[1]))
+            probs = exp_values / np.sum(exp_values)
+            action = np.random.choice(range(nb_actions), p=probs)
+        else:
+            action = np.argmax(q_values)
+        return action
+
+    def get_config(self):
+        config = super(MaxBoltzmannQPolicy, self).get_config()
+        config['eps'] = self.eps
+        config['tau'] = self.tau
+        config['clip'] = self.clip
+        return config

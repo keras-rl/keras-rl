397ff1ef330b5ce7256bcb7014d726c6a73d4e6d
Matthias Plappert <matthiasplappert@me.com>
2017-09-18 22:33:19 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl',
-      version='0.3.0',
+      version='0.3.1',
       description='Deep Reinforcement Learning for Keras',
       author='Matthias Plappert',
       author_email='matthiasplappert@me.com',

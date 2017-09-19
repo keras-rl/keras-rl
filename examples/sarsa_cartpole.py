a31e130157976592b4b8f6da75dd68c8fc77309c
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-09-20 00:41:50 +0530

    Increased nb_steps to 50000 for convergence

--- examples/sarsa_cartpole.py
+++ examples/sarsa_cartpole.py
@@ -38,7 +38,7 @@ sarsa.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-sarsa.fit(env, nb_steps=5000, visualize=False, verbose=2)
+sarsa.fit(env, nb_steps=50000, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 sarsa.save_weights('sarsa_{}_weights.h5f'.format(ENV_NAME), overwrite=True)

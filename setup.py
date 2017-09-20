68e5d863d38fd790b9b129d3a929f8716d8cf20d
Matthias Plappert <matthiasplappert@me.com>
2017-09-20 11:17:57 -0700

    Require keras < 2.0.7 due to non-trivial API changes (#134)
    * Require keras < 2.0.7 due to non-trivial API changes
    
    * Bump version
    
    * Change URL
    
    * Use pinned version for tests
    
    * Attempt to make Travis happy

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl',
-      version='0.3.0',
+      version='0.3.1',
       description='Deep Reinforcement Learning for Keras',
       author='Matthias Plappert',
       author_email='matthiasplappert@me.com',
       url='https://github.com/matthiasplappert/keras-rl',
-      download_url='https://github.com/matthiasplappert/keras-rl/archive/v0.3.0.tar.gz',
+      download_url='https://github.com/matthiasplappert/keras-rl/archive/v0.3.1.tar.gz',
       license='MIT',
-      install_requires=['keras>=1.0.7'],
+      install_requires=['keras>=1.0.7,<2.0.7'],
       extras_require={
           'gym': ['gym'],
       },

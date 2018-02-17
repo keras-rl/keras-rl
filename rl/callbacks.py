a4c4fdc45b2e9a4ee2c3990553547ec41a28d3a7
Amir <amir.h.abdi@gmail.com>
2018-02-16 18:59:35 -0800

    Add RlTensorBoard
    RlTensorBoard is a wrapper for TensorBoard callback in keras which works for Agents and RL

--- rl/callbacks.py
+++ rl/callbacks.py
@@ -356,3 +356,113 @@ class ModelIntervalCheckpoint(Callback):
         if self.verbose > 0:
             print('Step {}: saving model to {}'.format(self.total_steps, filepath))
         self.model.save_weights(filepath, overwrite=True)
+
+
+class RlTensorBoard(TensorBoard):
+    def __init__(self, log_dir='./logs',
+                 histogram_freq=0,
+                 batch_size=32,
+                 write_graph=True,
+                 write_grads=False,
+                 write_images=False,
+                 embeddings_freq=0,
+                 embeddings_layer_names=None,
+                 embeddings_metadata=None,
+                 agent=None):
+        super(RlTensorBoard, self).__init__(log_dir, histogram_freq, batch_size, write_graph, write_grads,
+                                            write_images, embeddings_freq, embeddings_layer_names,
+                                            embeddings_metadata)
+        self.agent = agent
+
+    def set_validation_data(self):
+        import numpy as np
+        experiences = self.agent.memory.sample(self.agent.batch_size)
+        # Start by extracting the necessary parameters (we use a vectorized implementation).
+        state0_batch = []
+        reward_batch = []
+        action_batch = []
+        terminal1_batch = []
+        state1_batch = []
+        for e in experiences:
+            state0_batch.append(e.state0)
+            state1_batch.append(e.state1)
+            reward_batch.append(e.reward)
+            action_batch.append(e.action)
+            terminal1_batch.append(0. if e.terminal1 else 1.)
+
+        # Prepare and validate parameters.
+        state0_batch = self.agent.process_state_batch(state0_batch)
+        state1_batch = self.agent.process_state_batch(state1_batch)
+        terminal1_batch = np.array(terminal1_batch)
+        reward_batch = np.array(reward_batch)
+        action_batch = np.array(action_batch)
+        assert reward_batch.shape == (self.batch_size,)
+        assert terminal1_batch.shape == reward_batch.shape
+        assert action_batch.shape == (self.agent.batch_size, self.agent.nb_actions)
+
+        # Compute Q values for mini-batch update.
+        q_batch = self.agent.target_V_model.predict_on_batch(state1_batch).flatten()
+        assert q_batch.shape == (self.batch_size,)
+
+        # Compute discounted reward.
+        discounted_reward_batch = self.agent.gamma * q_batch
+        # Set discounted reward to zero for all states that were terminal.
+        discounted_reward_batch *= terminal1_batch
+        assert discounted_reward_batch.shape == reward_batch.shape
+        Rs = reward_batch + discounted_reward_batch
+        assert Rs.shape == (self.agent.batch_size,)
+
+        self.validation_data = [action_batch, state0_batch, np.expand_dims(Rs, 1), np.ones(self.agent.batch_size)]
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.set_validation_data()
+        logs = logs or {}
+
+        if not self.validation_data and self.histogram_freq:
+            raise ValueError('If printing histograms, validation_data must be '
+                             'provided, and cannot be a generator.')
+        if self.validation_data and self.histogram_freq:
+            if epoch % self.histogram_freq == 0:
+                val_data = self.validation_data
+                tensors = (self.model.inputs +
+                           self.model.targets +
+                           self.model.sample_weights)
+                if self.model.uses_learning_phase:
+                    tensors += [K.learning_phase()]
+
+                assert len(val_data) == len(tensors)
+                val_size = val_data[0].shape[0]
+                i = 0
+                while i < val_size:
+                    step = min(self.batch_size, val_size - i)
+                    if self.model.uses_learning_phase:
+                        # do not slice the learning phase
+                        batch_val = [x[i:i + step] for x in val_data[:-1]]
+                        batch_val.append(val_data[-1])
+                    else:
+                        batch_val = [x[i:i + step] for x in val_data]
+                    assert len(batch_val) == len(tensors)
+                    feed_dict = dict(zip(tensors, batch_val))
+                    result = self.sess.run([self.merged], feed_dict=feed_dict)
+                    summary_str = result[0]
+                    self.writer.add_summary(summary_str, epoch)
+                    i += self.batch_size
+
+        if self.embeddings_freq and self.embeddings_ckpt_path:
+            if epoch % self.embeddings_freq == 0:
+                self.saver.save(self.sess,
+                                self.embeddings_ckpt_path,
+                                epoch)
+
+        for name, value in logs.items():
+            if name in ['batch', 'size']:
+                continue
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            try:
+                summary_value.simple_value = value.item()
+            except:
+                summary_value.simple_value = value
+            summary_value.tag = name
+            self.writer.add_summary(summary, epoch)
+        self.writer.flush()

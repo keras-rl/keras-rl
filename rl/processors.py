c3d21119512187e5f72e5cf086de239e1388ff02
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2018-05-10 18:09:40 +0200

    debugging, adding multidimensional support
    previously only 2D inputs (matrices) could be handled, now alll kind of inputs, vectors, 2D matrices or higher dimensions should be handled

--- rl/processors.py
+++ rl/processors.py
@@ -7,7 +7,6 @@ from rl.util import WhiteningNormalizer
 class MultiInputProcessor(Processor):
     """Converts observations from an environment with multiple observations for use in a neural network
     policy.
-
     In some cases, you have environments that return multiple different observations per timestep 
     (in a robotics context, for example, a camera may be used to view the scene and a joint encoder may
     be used to report the angles for each joint). Usually, this can be handled by a policy that has
@@ -15,7 +14,6 @@ class MultiInputProcessor(Processor):
     in the form of a tuple `[(modality1_t, modality2_t, ..., modalityn_t) for t in T]` but the neural network
     expects them in per-modality batches like so: `[[modality1_1, ..., modality1_T], ..., [[modalityn_1, ..., modalityn_T]]`.
     This processor converts observations appropriate for this use case.
-
     # Arguments
         nb_inputs (integer): The number of inputs, that is different modalities, to be used.
             Your neural network that you use for the policy must have a corresponding number of
@@ -44,17 +42,20 @@ class MultiInputProcessor(Processor):
         names = state_batch[0][0].keys()
         ordered_dict = dict()
         for key in names: 
-            order = np.zeros((len(state_batch), 
-                              state_batch[0][0][key].shape[0], 
-                              state_batch[0][0][key].shape[1], 
-                              state_batch[0][0][key].shape[2]))
+            dim = len(state_batch[0][0][key].shape)
+            order_dim = (len(state_batch),)
+            for dim_count in range(dim): order_dim = order_dim + (state_batch[0][0][key].shape[dim_count],)
+            order = np.zeros(order_dim)
+
+
             missinglayer=0 
             for j in range(len(state_batch)): 
                 for i in range(order.shape[1]): 
                     assert len(state_batch[j][missinglayer]) == self.nb_inputs
                     order[j, i] = state_batch[j][missinglayer][key][i][0]
             ordered_dict[key] = order
-return ordered_dict
+        return ordered_dict
+
 
 class WhiteningNormalizerProcessor(Processor):
     """Normalizes the observations to have zero mean and standard deviation of one,

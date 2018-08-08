94ac5395e477afca02654e2fa1863da4cfd4af21
luffy <rajrohan1108@gmail.com>
2018-08-08 16:40:48 +0530

    Working without memory and trust region

--- rl/agents/acer.py
+++ rl/agents/acer.py
@@ -3,62 +3,101 @@ import warnings
 from collections import namedtuple
 # import multiprocessing as mp
 
+import numpy as np
 import keras.backend as K
 import keras.optimizers as optimizers
-from keras.models import Model
-from keras.layers import Lambda, Input, Layer, Dense
 
 from rl.core import Agent
 from rl.util import *
+from time import time
 
 # TODO : Add different warnings and exceptions
 
 Transition = namedtuple('Transition', ('state', 'action', 'reward', 'done', 'mus'))
 
-def mean_q(y_true, y_pred):
-    return K.mean(K.max(y_pred, axis=-1))
+def returns(R):
+    assert (len(K.int_shape(R))==1)
+    return K.mean(R, axis=0, keepdims=False)
+
+def q_retrace(R, D, q_i, v, rho_i, gamma):
+    """
+    Calculates q_retrace targets
+    :param R: Rewards
+    :param D: Dones
+    :param q_i: Q values for actions taken
+    :param v: V values
+    :param rho_i: Importance weight for each action
+    :return: Q_retrace values
+    """
+    assert (len(K.int_shape(R)) == 1)
+    assert (len(K.int_shape(D)) == 1)
+    rho_bar = K.clip(rho_i,min_value=0, max_value=1.0)
+    qrets = []
+    qret = v[-1] * (1 - D[-1])
+    l = K.int_shape(R)[0]
+    for i in reversed(range(l)):
+        qret = R[i] + gamma * qret * (1 - D[i])
+        qrets.append(qret)
+        qret = (rho_bar[i] * (qret - q_i[i])) + v[i]
+    return qrets
+
+def get_by_index(x, idx, shape=None):
+    if shape is None:
+        shape = K.int_shape(x)
+    idx_flattened = K.arange(0, shape[0]) * shape[1] + idx
+    y = K.gather(K.reshape(x, [-1]),  # flatten input
+                 idx_flattened)  # use flattened indices
+    return y
 
 class ACERAgent(Agent):
-    def __init__(self, model, nb_actions, gamma=0.99, batch_size=32,
-                 on_policy=False, replay_ratio = 4, num_process=1, replay_start=2000, 
+    def __init__(self, model_fn, nb_actions, obs_shape, policy, gamma=0.99, nenvs=1, memory_interval=1,
+                 on_policy=False, replay_ratio = 4, num_process=1, replay_start=200, nb_warmup_steps = 40,
                  trace_decay=1, trace_max=10, trust_region=True, trust_region_decay=0.99, 
-                 trust_region_thresold = 1., lr=1e-5, rmsprop_decay = 0.99, nsteps = 20,
+                 trust_region_thresold = 1., nsteps = 20, eps= 1e-5,
                  entropy_weight = 1e-4, value_weight = 0.5,max_gradient_norm = 40, **kwargs):
         super(ACERAgent, self).__init__(**kwargs)         
 
         # Parameters
         self.nb_actions = nb_actions
-        # self.memory = memory
+        self.obs_shape = obs_shape
         self.gamma = gamma
-        self.batch_size = 32
+        self.policy = policy
         self.on_policy = on_policy
         self.replay_ratio = replay_ratio
-        # self.replay_start = replay_start
+        self.replay_start = replay_start
         self.num_process = num_process
-        # self.trace_max = trace_max
-        # self.trace_decay = trace_decay
-        # self.trust_region_decay = trust_region_decay
-        # self.trust_region_thresold = trust_region_thresold
-        self.lr = lr
-        # self.rmsprop_decay = rmsprop_decay
+        self.trace_max = trace_max
+        self.trace_decay = trace_decay
+        self.trust_region_decay = trust_region_decay
+        self.trust_region_thresold = trust_region_thresold
         self.entropy_weight = entropy_weight
         self.value_weight = value_weight
         self.max_gradient_norm = max_gradient_norm
         self.nsteps = nsteps
-        self.nenv = 1
+        self.nenvs = nenvs
+        self.eps = eps
+        self.memory_interval = memory_interval
+        self.nb_warmup_steps = 40
         # Model        
-        self.model = model
 
-        self.nbatch = nenv * nsteps
+        self.model, _ , _ = self.make_model(self.nsteps, model_fn)
+        self.step_model, self.step_model_input, self.step_model_output = self.make_model(1, model_fn, 'step_input')
+        self.step_fn = self.make_step_function()
+        self.update_step_model_weights()
+
+        self.nbatch = self.nenvs * self.nsteps
         self.trajectory = []
         
         self.t_start = 0
 
+        self.terminal = False
+
+        self.compiled = False
         self.reset_states()
 
-    @property
+@property
     def uses_learning_phase(self):
-        return self.actor.uses_learning_phase or self.critic.uses_learning_phase
+        return self.model.uses_learning_phase
 
     def compile(self, optimizer, metrics=[]):
 
@@ -70,43 +109,47 @@ class ACERAgent(Agent):
         if type(optimizer) is str:
             self.optimizer = optimizers.get(optimizer)
 
-        # TODO : Check metrics part
-        # if len(metrics) == 2 and hasattr(metrics[0], '__len__') and hasattr(metrics[1], '__len__'):
-        #     actor_metrics, critic_metrics = metrics
-        # else:
-        #     actor_metrics = critic_metrics = metrics
+        if len(metrics) != 0:
+            raise ValueError('Please add your metrics to the computation graph. Current implementation supports None')
 
-        mus = K.placeholder(shape=(self.nbatch, self.nb_actions))
+        self.model.metrics_names = ['Avg_Rewards']
+
+        print ('Compiling the Agent')
+        print (K.backend())
+        inp = self.model.input
+        Q, mus = self.model(inp)
+
         old_mus = K.placeholder(shape=(self.nbatch, self.nb_actions))
-        Q = K.placeholder(shape=(self.nbatch, self.nb_actions))
-        A = K.placeholder(shape=[self.nbatch])
+        A = K.placeholder(shape=[self.nbatch], dtype='int32')
         R = K.placeholder(shape=[self.nbatch])
         D = K.placeholder(shape=[self.nbatch])
+        rho = mus/old_mus
 
-        V = K.mean(Q, axis=1)
+        V = K.mean(mus*Q, axis=1, keepdims=False)
+
+        Q_i = get_by_index(Q, A)
+        rho_i = get_by_index(rho, A, shape=(self.nbatch, self.nb_actions))
+
+        Qret = q_retrace(R, D, Q_i, V, rho_i, self.gamma)
         f_i = get_by_index(mus, A)
-        Q_i = get_by_index(Qs, A)
-
-        # if on_policy rho is 1
-        rho = K.stop_gradient(mus/old_mus)
-        rho_i = get_by_index(rho, A)
-
-        Qret = self.q_retrace(R, D, Q_i, V)
+        # print ('Qret and f_i calculated')
 
         # Policy gradient loss
-        adv = Qret - Vs
-        logf_i = K.log(f_i + self.eps)
-        gain_f = logf * K.stop_gradient(adv * K.clip(rho_i, max_value=self.c))
+        adv = Qret - V
+        logf = K.log(f_i + self.eps)
+        gain_f = logf * K.stop_gradient(adv * K.clip(rho_i, min_value=0, max_value=self.trace_max))
         loss_f = -K.mean(gain_f)
+        # print ('Loss f calculated')
 
         # Bias correction for the truncation
         adv_bc = Q - K.reshape(V, (self.nbatch, 1))
         logf_bc = K.log(mus + self.eps)
-        gain_bc = K.sum(logf_bc * K.stop_gradient(adv_bc * K.relu(1. - self.c / (rho + self.eps)) * mus), axis=1)
+        gain_bc = K.sum(logf_bc * K.stop_gradient(adv_bc * K.relu(1. - self.trace_max / (rho + self.eps)) * mus), axis=1)
         loss_bc = -K.mean(gain_bc)
 
         loss_policy = loss_bc + loss_f
-        
+        # print ('policy loss calculated')
+
         # Add Entropy
         entropy = -K.mean(K.sum(K.log(mus)*mus, axis=1))
 
@@ -115,10 +158,10 @@ class ACERAgent(Agent):
 
         total_loss = loss_policy + self.value_weight*loss_value
 
-        inputs = []
-        for i in self.model.input:
-            inputs.append(i)
-        inputs = inputs + [mus, old_mus,  Q, A, R, D]
+        inputs = [inp]
+        inputs = inputs + [old_mus, A, R, D]
+        # metrics = returns(R)
+        metrics = K.mean(f_i * R)
 
         # Add trust region
 
@@ -127,44 +170,57 @@ class ACERAgent(Agent):
         # Finally, combine it all into a callable function.
         if K.backend() == 'tensorflow':
             self.train_fn = K.function(inputs + [K.learning_phase()],
-                                             [self.actor(inputs)], updates=updates)
+                                       [metrics], updates=updates)
         else:
             if self.uses_learning_phase:
                 inputs += [K.learning_phase()]
-            self.train_fn = K.function(inputs, [self.actor(inputs)], updates=updates)
+            self.train_fn = K.function(inputs, [metrics], updates=updates)
+        print ('Agent Compiled')
         self.compiled = True
 
+    def make_model(self, nsteps, model_fn, name=None):
+        shape = (self.nenvs * nsteps,) + self.obs_shape
+        inp = K.placeholder(shape=shape)
+        if name == None:
+            return model_fn(inp)
+        else:
+            return model_fn(inp, name)
 
+    def make_step_function(self):
+        # inp = self.step_model.input
+        # out = self.step_model(inp)
+        return K.function(inputs=self.step_model_input, outputs=self.step_model_output)
+
+    def update_step_model_weights(self):
+        self.step_model.set_weights(self.model.get_weights())
+
+    
     def forward(self, observation):
         # Select an action.
-        state = self.memory.get_recent_state(observation)
-        mus = self.actor(state)  
-
+        state = np.asarray([observation], dtype=np.float32)
+        _, mus = self.step_fn([state])
         self.recent_observation = observation
-        self.recent_mus = mus
-        action = self.policy.select_action(actions)
-
+        # To deal with error : ValueError: probabilities do not sum to 1
+        self.recent_mus = mus[0]/sum(mus[0])
+        action = self.policy.select_action(self.nb_actions, self.recent_mus)
+        self.recent_action = action
         return action
 
     @property
     def metrics_names(self):
-        names = self.critic.metrics_names[:]
+        names = self.model.metrics_names[:]
         return names
 
     def load_weights(self, filepath):
         filename, extension = os.path.splitext(filepath)
-        actor_filepath = filename + '_actor' + extension
-        critic_filepath = filename + '_critic' + extension
-        self.actor.load_weights(actor_filepath)
-        self.critic.load_weights(critic_filepath)
+        model_filepath = filename + '_acer_model' + extension
+        self.model.load_weights(model_filepath)
         self.update_target_models_hard()
 
     def save_weights(self, filepath, overwrite=False):
         filename, extension = os.path.splitext(filepath)
-        actor_filepath = filename + '_actor' + extension
-        critic_filepath = filename + '_critic' + extension
-        self.actor.save_weights(actor_filepath, overwrite=overwrite)
-        self.critic.save_weights(critic_filepath, overwrite=overwrite)
+        model_filepath = filename + '_acer_model' + extension
+        self.model.save_weights(model_filepath, overwrite=overwrite)
 
     # TODO : Check reset states
     def reset_states(self):
@@ -175,24 +231,17 @@ class ACERAgent(Agent):
 
     @property
     def layers(self):
-        return self.actor.layers[:] + self.critic.layers[:]
+        return self.model.layers[:]
 
     @property
     def metrics_names(self):
-        names = self.critic.metrics_names[:]
+        names = self.model.metrics_names[:]
         if self.processor is not None:
             names += self.processor.metrics_names[:]
         return names
 
     def backward(self, reward, terminal=False):
 
-        if self.step % self.memory_interval == 0 or self.terminal:
-            if self.terminal:
-                self.terminal = False
-            else:
-                self.trajectory.append(Transition(self.recent_observation, self.recent_action,
-                                       reward, terminal, self.recent_mus))
-                self.t_start = self.t_start + 1
 
         metrics = [np.nan for _ in self.metrics_names]
 
@@ -202,6 +251,15 @@ class ACERAgent(Agent):
             self.trajectory = []
             return metrics
 
+        if self.step % self.memory_interval == 0 or self.terminal:
+            if self.terminal:
+                self.terminal = False
+                return metrics
+            else:
+                self.trajectory.append(Transition(self.recent_observation, self.recent_action,
+                                       reward, terminal, self.recent_mus))
+                self.t_start = self.t_start + 1
+
         if terminal:
             self.terminal = True
         
@@ -210,14 +268,16 @@ class ACERAgent(Agent):
 
         if self.t_start % self.nsteps == 0:
             assert len(self.trajectory) == self.nsteps
-            obs, mus, old_mus, Q, A, R, D = [], [], [], [], [], [], []
+            obs, old_mus, A, R, D = [], [], [], [], []
             for i in range(len(self.trajectory)):
-                obs.append(trajectory[i].state)
-                mus.append(trajectory[i].mus)
-                old_mus.append(trajectory[i].mus)
-                Q.append(self.model(K.variable([trajectory[i]]))[0,0])
-                A.append(trajectory[i].action)
-                R.append(trajectory[i].reward)
-                D.append(trajectory[i].done)
-            self.train_fn([obs, mus, old_mus,  Q, A, R, D])
-            print("Trajectory " , self.t_start / self.nsteps, " optimised")
+                action = self.trajectory[i].action
+                obs.append(self.trajectory[i].state)
+                # TODO : Add off policy
+                old_mus.append(self.trajectory[i].mus)
+                A.append(action)
+                R.append(self.trajectory[i].reward)
+                D.append(self.trajectory[i].done)
+            self.train_fn([obs, old_mus, A, R, D])
+            self.update_step_model_weights()
+            self.trajectory = []
+        return metrics

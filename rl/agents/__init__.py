c4c3e95b389d7838553775fe6d208cd553d0c77f
luffy <rajrohan1108@gmail.com>
2018-08-08 16:42:15 +0530

    Added ACER

--- rl/agents/__init__.py
+++ rl/agents/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
+from .acer import ACERAgent
 from .dqn import DQNAgent, NAFAgent, ContinuousDQNAgent
 from .ddpg import DDPGAgent
 from .cem import CEMAgent
 from .sarsa import SarsaAgent, SARSAAgent
-from .acer import ACERAgent

e7bc278d1b0f726235aa3ae80b414d88da39d53c
Yifan Wang <ericwang.usa@gmail.com>
2020-01-31 14:14:52 +0900

    Change model.output to model.outputs to solve the undefined len issue.

--- rl/agents/ddpg.py
+++ rl/agents/ddpg.py
@@ -26,6 +26,10 @@ class DDPGAgent(Agent):
                  gamma=.99, batch_size=32, nb_steps_warmup_critic=1000, nb_steps_warmup_actor=1000,
                  train_interval=1, memory_interval=1, delta_range=None, delta_clip=np.inf,
                  random_process=None, custom_model_objects={}, target_model_update=.001, **kwargs):
+        if len(actor.outputs) > 1:
+            raise ValueError('Actor "{}" has more than one output. DDPG expects an actor that has a single output.'.format(actor))
+        if len(critic.outputs) > 1:
+            raise ValueError('Critic "{}" has more than one output. DDPG expects a critic that has a single output.'.format(critic))
         if critic_action_input not in critic.input:
             raise ValueError('Critic "{}" does not have designated action input "{}".'.format(critic, critic_action_input))
         if not hasattr(critic.input, '__len__') or len(critic.input) < 2:
